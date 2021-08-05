5e31db60328a82f04870200550f3692057366940
tangli <tangli@idriverplus.com>
2021-08-05 13:18:42 +0800

    增加lottie图标2D动画效果

--- app/src/main/java/com/example/sztangli/widgedemo/lottie/LottieFragment.java
+++ app/src/main/java/com/example/sztangli/widgedemo/lottie/LottieFragment.java
@@ -1,9 +1,16 @@
 package com.example.sztangli.widgedemo.lottie;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.airbnb.lottie.LottieComposition;
+import com.airbnb.lottie.LottieCompositionFactory;
+import com.airbnb.lottie.LottieDrawable;
+import com.airbnb.lottie.LottieListener;
+import com.airbnb.lottie.LottieTask;
 import com.example.sztangli.widgedemo.R;
 import com.example.mvvmlibrary.base.data.BaseViewModel;
+import com.example.sztangli.widgedemo.base.app.WidgetDemoApplication;
 import com.example.sztangli.widgedemo.databinding.FragmentLottieBinding;
 import com.guyuan.handlein.base.ui.fragment.BaseFragment;
 
@@ -22,7 +29,24 @@ public class LottieFragment extends BaseFragment<FragmentLottieBinding, BaseView
 
     @Override
     protected void initialization() {
+        LottieDrawable lottieDrawable = new LottieDrawable();
+        LottieTask<LottieComposition> lottieTask = LottieCompositionFactory.
+                fromRawRes(WidgetDemoApplication.getInstance(), R.raw.type1);
+        lottieTask.addListener(new LottieListener<LottieComposition>() {
+            @Override
+            public void onResult(LottieComposition result) {
+                lottieDrawable.setComposition(result);
+                binding.iv.setImageDrawable(lottieDrawable);
+                lottieDrawable.start();
+            }
+        });
 
+        binding.iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lottieDrawable.start();
+            }
+        });
     }
 
     @Override
